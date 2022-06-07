2279cd5113a30a75271d46ca99cfd1e8522b8502
soulchicken <soulfever01@naver.com>
2022-06-07 23:37:00 +0900

    FIX : 각자 로컬에서 사용할 수 있도록 수정
    - 텅빈 repository 파일이 있으면 서버가 가동되지 않아서 일단 지움 (각자 알아서 추가해야할 듯)
    - 연결하는 DB를 mydb로 설정

--- src/main/java/com/dev/covid/repository/HospitalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dev.covid.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.covid.model.Hospital;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Long>{
-
-}
--- src/main/java/com/dev/covid/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dev.covid.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.covid.model.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Long>{
-
-}
