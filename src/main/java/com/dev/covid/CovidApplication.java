8f15fc1e797a37b74a18657a4545854e85654b7f
soulchicken <soulfever01@naver.com>
2022-06-08 00:47:14 +0900

    FEAT : 자가격리자 model 완성
    - 자가격리자 (self Quarantine) 완성
    - 테이블 생성까지는 됨
    - junit으로 테스트를 못하는 중

--- src/main/java/com/dev/covid/CovidApplication.java
+++ src/main/java/com/dev/covid/CovidApplication.java
@@ -1,13 +1,45 @@
 package com.dev.covid;
 
+import java.text.SimpleDateFormat;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.dev.covid.model.SelfQuarantine;
 
 @SpringBootApplication
 public class CovidApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovidApplication.class, args);
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("covid");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			tx.begin();
+			SelfQuarantine selfQuarantine = new SelfQuarantine();
+			selfQuarantine.setSelfQuarantinename("김치킨");
+			selfQuarantine.setSelfQuarantinephoneNumber("01012341234");
+			selfQuarantine.setSelfQuarantineDate(formatter.parse("2022-06-01"));
+			selfQuarantine.setSelfQuarantineRelease(formatter.parse("2202-06-08"));
+			em.persist(selfQuarantine);
+			System.out.println(selfQuarantine);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
+			emf.close();
+		}
+		
 	}
 
 }
---
+++ src/main/java/com/dev/covid/model/SelfQuarantine.java
@@ -0,0 +1,99 @@
+package com.dev.covid.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="self_quarantine")
+public class SelfQuarantine {
+	
+// 지금은 아이디값 1씩 자동증가로 해놨지만 이후에 OneToOne으로 전환 
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@OneToOne
+//	@JoinColumn(mappedBy = "selfQuarantine")
+//	private Patient patient;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "patient_people_id")
+	private long patientPeopleId;
+	
+	@Column(name = "self_quarantine_date")
+	private Date selfQuarantineDate;
+	
+	public long getPatientPeopleId() {
+		return patientPeopleId;
+	}
+
+	public void setPatientPeopleId(long patientPeopleId) {
+		this.patientPeopleId = patientPeopleId;
+	}
+
+	public Date getSelfQuarantineDate() {
+		return selfQuarantineDate;
+	}
+
+	public void setSelfQuarantineDate(Date selfQuarantineDate) {
+		this.selfQuarantineDate = selfQuarantineDate;
+	}
+
+	public Date getSelfQuarantineRelease() {
+		return selfQuarantineRelease;
+	}
+
+	public void setSelfQuarantineRelease(Date selfQuarantineRelease) {
+		this.selfQuarantineRelease = selfQuarantineRelease;
+	}
+
+	public String getSelfQuarantinename() {
+		return selfQuarantinename;
+	}
+
+	public void setSelfQuarantinename(String selfQuarantinename) {
+		this.selfQuarantinename = selfQuarantinename;
+	}
+
+	public String getSelfQuarantinephoneNumber() {
+		return selfQuarantinephoneNumber;
+	}
+
+	public void setSelfQuarantinephoneNumber(String selfQuarantinephoneNumber) {
+		this.selfQuarantinephoneNumber = selfQuarantinephoneNumber;
+	}
+
+	@Column(name = "self_quarantine_release")
+	private Date selfQuarantineRelease;
+	
+	@Column(name = "self_quarantine_people_name")
+	private String selfQuarantinename;
+	
+	@Column(name = "self_quarantine_phone_number")	
+	private String selfQuarantinephoneNumber;
+
+	@Override
+	public String toString() {
+		return "SelfQuarantine [patientPeopleId=" + patientPeopleId + ", selfQuarantineDate=" + selfQuarantineDate
+				+ ", selfQuarantineRelease=" + selfQuarantineRelease + ", selfQuarantinename=" + selfQuarantinename
+				+ ", selfQuarantinephoneNumber=" + selfQuarantinephoneNumber + "]";
+	}
+
+	
+	
+}
