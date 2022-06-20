40fa3cee01cf40a7b14b4e4f41878a25a00c03df
Hyeonjin <hyunjin445@gmail.com>
2022-06-20 10:53:48 +0900

    FEAT : CRUD 기능 추가

--- src/main/java/com/dev/covid/controller/ManagerController.java
+++ src/main/java/com/dev/covid/controller/ManagerController.java
@@ -1,5 +1,46 @@
 package com.dev.covid.controller;
 
-public class ManagerController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.covid.model.Manager;
+import com.dev.covid.service.ManagerService;
+
+@RestController
+@RequestMapping("manager")
+public class ManagerController {
+	
+	@Autowired
+	private ManagerService service;
+	
+	@GetMapping
+	public List<Manager> findAll(){
+		return service.findAll();
+	}
+	
+	@PostMapping
+	public Manager save(@RequestBody Manager manager) {
+		return service.save(manager);
+	}
+	
+	@PutMapping
+	public List<Manager> put(@RequestBody Manager manager) {
+		return service.put(manager);
+	}
+	
+	@DeleteMapping("/{id}")
+	public List<Manager> delete(@PathVariable("id") Long id) {
+		return service.delete(id);
+	}
+	
+	
 }
--- src/main/java/com/dev/covid/service/ManagerService.java
+++ src/main/java/com/dev/covid/service/ManagerService.java
@@ -1,5 +1,47 @@
 package com.dev.covid.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.covid.model.Manager;
+import com.dev.covid.repository.ManagerRepository;
+
+@Service
 public class ManagerService {
+	
+	@Autowired
+	private ManagerRepository repository;
+	
+	public List<Manager> findAll(){
+		return repository.findAll();
+	}
+
+	public Manager save(Manager manager) {
+		return repository.save(manager);
+	}
+
+	public List<Manager> put(Manager manager) {
+		final Optional<Manager> foundManager = repository.findById(manager.getManagerId());
+		foundManager.ifPresent(updateManager -> {
+			updateManager.setManagerId(manager.getManagerId());
+			updateManager.setManagerName(manager.getManagerName());
+			updateManager.setManagerPhone(manager.getManagerPhone());
+			
+			repository.save(updateManager);
+		});
+		return repository.findAll();
+	}
+
+	public List<Manager> delete(Long id) {
+		final Optional <Manager> foundManager = repository.findById(id);
+		foundManager.ifPresent(manager -> {
+			repository.delete(manager);
+		});
+		return repository.findAll();
+	}
+
 
 }
