91f41f8036dea2d8fd60393cabb00785e31d683c
소울치킨 <soulfever01@naver.com>
2022-06-25 15:12:43 +0900

    test : 감염경로 전체 개수를 test 로 조회하기

--- src/test/java/com/dev/covid/model/InfectionTrackingTest.java
+++ src/test/java/com/dev/covid/model/InfectionTrackingTest.java
@@ -2,27 +2,37 @@ package com.dev.covid.model;
 
 import com.dev.covid.DTO.InfectionTrackingDTO;
 import com.dev.covid.common.BaseControllerTest;
+import com.dev.covid.repository.InfectionTrackingRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 @Slf4j
 @DisplayName("감염경로 테스트")
 public class InfectionTrackingTest extends BaseControllerTest {
+
+    String url = "/infectiontracking";
+    
+    @Autowired
+    InfectionTrackingRepository infectionTrackingRepository;
+    
+    @Test
+    @DisplayName("감염경로 전체 조회 (성공)")
+    void findAllTest() throws Exception {
+         List<InfectionTracking> result = infectionTrackingRepository.findAll();
+        log.info("감염경로 전체 개수 : " + result.size());
+    }
+
     @Test
     @DisplayName("포스트 저장(성공)")
     void insertPostSuccess() throws Exception {
-        String url = "/infectiontracking";
-//        {
-//            "infectionTrackingName":"이거 머임",
-//                "infectionTrackingArea":"뚝섬",
-//                "infectionTrackingDate":"2022-06-24",
-//                "infectionTrackingCause":"모임",
-//                "patientPeopleId":1
-//        }
+
         InfectionTrackingDTO infectionTrackingDTO = InfectionTrackingDTO
                 .builder()
                 .infectionTrackingName("이거 머임")
@@ -39,5 +49,4 @@ public class InfectionTrackingTest extends BaseControllerTest {
                     log.info(response.getContentAsString());
                 });
     }
-
 }
