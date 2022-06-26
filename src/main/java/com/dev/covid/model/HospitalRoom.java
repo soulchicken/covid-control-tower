90772b6813ae055b45fb8219fa3e9fe758aa5c27
소울치킨 <soulfever01@naver.com>
2022-06-27 02:53:45 +0900

    fix : 조금씩 더 그럴싸하게 만드는 중입니다

--- src/main/java/com/dev/covid/model/HospitalRoom.java
+++ src/main/java/com/dev/covid/model/HospitalRoom.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Entity
 @Table(name = "hospitalroom")
 public class HospitalRoom {
+    
     @Id
     @Column(name = "hospitalroom_roomnumber")
     private Long hospitalroomRoomnumber;
